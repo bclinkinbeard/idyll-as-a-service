fafa6157c0521748c2d4f1fe10cf0d9a35bb06d0
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-05-25 16:52:27 -0400

    Separate builds by user, GET or POST, accept minify arg

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const uuid = require('uuid/v4')
 
 const idyll = require('idyll')
+if (!fs.existsSync('tmp')) fs.mkdirSync('tmp')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use(cors())
 
 app.set('x-powered-by', false)
 app.set('json spaces', 2)
 
-app.get('/', (req, res) => {
-  if (!req.query.src) return res.send('hi')
+const handleRequest = (req, res) => {
+  const input = req.method === 'GET' ? req.query : req.body
+  if (!input.src) res.send('East does it, you need to send a `src` property!')
+
+  const id = req.cookies.id || uuid()
+  if (!req.cookies.id) res.cookie('id', id)
 
   idyll({
-    inputString: req.query.src,
-    output: 'tmp',
-    minify: false,
+    output: path.join('tmp', id),
+    minify: input.minify ? input.minify === 'true' : false,
     debug: true,
     compilerOptions: {spellcheck: false}
   })
   .once('update', (output) => {
     res.json(output)
   })
-  .build(req.query.src)
-})
+  .build(input.src)
+}
 
-app.post('/', (req, res) => {
-  idyll({
-    inputString: req.body.src,
-    output: 'tmp',
-    minify: false,
-    debug: true,
-    compilerOptions: {spellcheck: false}
-  })
-  .once('update', (output) => {
-    res.json(output)
-  })
-  .build(req.body.src)
-})
+app.get('/', handleRequest)
+app.post('/', handleRequest)
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log('Idyll as a service, at your service!')
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+  console.log(`Idyll as a service, at your service on port ${port}!`)
 })
